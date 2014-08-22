c3f2b46513be2574f2d4f9b0c1df75baa03cb690
ronen <narkisr@gmail.com>
2014-08-23 02:15:09 +0300

    question extraction works

--- project.clj
+++ project.clj
@@ -4,5 +4,10 @@
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [
       [org.clojure/clojure "1.5.1"]
-      [dnsjava "2.1.6"]
-  ])
+      [aleph "0.3.3"]
+      [dnsjava/dnsjava "2.1.6"] 
+   ]
+  
+  :main nadir.core
+
+)
--- src/nadir/core.clj
+++ src/nadir/core.clj
@@ -1,3 +1,32 @@
-(ns nadir.core)
-
-
+(ns nadir.echo
+  (:import 
+    org.xbill.DNS.Message (java.net InetAddress DatagramPacket DatagramSocket)))
+ 
+(def udp-server (ref nil))
+ 
+(def port 1234)
+ 
+(defn localhost [] (. InetAddress getLocalHost))
+ 
+(defn create-udp-server []
+  (DatagramSocket. port))
+ 
+(defn start-udp-server []
+  (dosync (ref-set udp-server (create-udp-server))))
+ 
+(defn stop-udp-server []
+  (.close @udp-server))
+ 
+(start-udp-server)
+ 
+(defn empty-message [n] 
+  (new DatagramPacket (byte-array n) n))
+ 
+(defn -main []
+  (while true
+    (let [pkt (empty-message 1024)]
+      (.receive @udp-server pkt)
+      (let [record (.getQuestion (Message. (.getData pkt)))
+            label (.toString (.getName record) true)]
+      (println label))))
+  )
