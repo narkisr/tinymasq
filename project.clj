367e7e6d28d953e1d9d9d5e8c5ef4b610c950c2a
narkisr <narkisr@gmail.com>
2014-08-22 03:15:30 +0300

    initial import

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject nadir "0.0.1"
+  :description "A malable DNS forwarder"
+  :url ""
+  :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [
+      [org.clojure/clojure "1.5.1"]
+      [dnsjava "2.1.6"]
+  ])
