af44db4a3c4c5c2f6505b26f43fae7fd44cb6534
ronen <narkisr@gmail.com>
2014-08-23 22:18:45 +0300

    set and get works

--- src/tinymasq/store.clj
+++ src/tinymasq/store.clj
@@ -1,8 +1,10 @@
 (ns tinymasq.store
   "host lookup"
   (:require  
+    [clojure.string :refer (split)]
+    [taoensso.carmine :as car :refer  (wcar)]
     [tinymasq.config :refer (tiny-config)]
-    [clojure-watch.core :refer  [start-watch]])
+    )
   )
 
 (def records (ref {:bla.com. [1 2 3 4]}))
@@ -12,6 +14,19 @@
 (defmacro wcar* [& body] 
   `(car/wcar server-conn ~@body))
 
+(defn into-bytes
+  "converting an ip 1.2.3.4 into bytes array [1 2 3 4]"
+   [ip]
+   (when ip
+     (byte-array (mapv #(.byteValue (Integer. %)) (split ip #"\.")))))
+
 (defn lookup [host]
-  (@records (keyword host)))
+  (into-bytes (wcar* (car/get host))))
+
+(defn add-host 
+  "Adding hostname -> ip" 
+  [host ip]
+  (wcar* (car/set host ip)))
+
+;; (add-host "bla.com." "255.127.1.10")
 
