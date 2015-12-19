0befb54dd51a16f7775655c828d5d61b354c4e03
Ingomar Otter <ingomar.otter@valtech.de>
2015-12-19 20:38:39 +0100

    clj deps

--- project.clj
+++ project.clj
@@ -4,24 +4,24 @@
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [
       [org.clojure/clojure "1.6.0"]
-      [com.taoensso/carmine "2.7.0"] 
-      [dnsjava/dnsjava "2.1.6"]
-      [com.taoensso/timbre "3.3.1"]
+      [com.taoensso/carmine "2.12.1"]
+      [dnsjava/dnsjava "2.1.7"]
+      [com.taoensso/timbre "4.1.4"]
       [org.clojure/core.incubator "0.1.3"]
-      [org.clojure/core.cache "0.6.3"] 
-      [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+      [org.clojure/core.cache "0.6.4"]
+      [org.clojure/core.async "0.2.374"]
       ; http api
       [ring/ring-ssl "0.2.1"]
-      [ring-middleware-format "0.4.0"]
-      [ring/ring-jetty-adapter "1.3.1"]
-      [com.cemerick/friend "0.2.1"] 
-      [ring "1.3.1"]
-      [compojure "1.1.9" :exclusions  [ring/ring-core]]]
-  
+      [ring-middleware-format "0.7.0"]
+      [ring/ring-jetty-adapter "1.4.0"]
+      [com.cemerick/friend "0.2.1"]
+      [ring "1.4.0"]
+      [compojure "1.4.0" :exclusions  [ring/ring-core]]]
+
   :exclusions [org.clojure/clojure org.clojure/core.cache]
   :profiles {
     :dev {
-      :dependencies [[midje "1.6.3"]]
+      :dependencies [[midje "1.8.3"]]
       :plugins [
           [lein-set-version "0.3.0"]
           [lein-midje "3.1.3"]
@@ -44,7 +44,7 @@
   }
 
   :aot [tinymasq.core tinymasq.import tinymasq.genhash tinymasq.bench]
- 
+
   :main tinymasq.core
 
 )
