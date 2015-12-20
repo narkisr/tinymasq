2c4490d33b0aeefe9b988e8d9cb5984fd1da3c4f
Ingomar Otter <ingomar.otter@valtech.de>
2015-12-20 12:56:38 +0100

    adjusted tests

--- src/tinymasq/store.clj
+++ src/tinymasq/store.clj
@@ -1,14 +1,3 @@
-(comment
-  Tinymasq, Copyright 2013 Ronen Narkis, narkisr.com
-  Licensed under the Apache License,
-  Version 2.0  (the "License") you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.)
-
 (ns tinymasq.store
   "host lookup"
   (:require
@@ -32,23 +21,40 @@
     (throw (Exception. (<< "Failed to ~{action} ~{host}")))))
 
 (defn get-host
-  "get host ip"
-  [host]
+  "get host ip, given name and auth-token"
+  [auth host]
   (wcar* (car/get host)))
 
 (defn add-host
-  "Adding hostname -> ip"
-  [host ip]
+  "Adding hostname -> ip, auth is the authentication-token
+   that is required for follow up operations,"
+  [auth host ip & [description]]
   {:post [(assert-op "add" host %)]}
   (wcar* (car/set host ip "NX")))
 
 (defn update-host
   "Update hostname -> ip"
-  [host ip]
+  [auth host ip &[description]
   {:post [(assert-op "add" host %)]}
   (wcar* (car/set host ip "XX")))
 
 (defn del-host
   "Deleting a host"
-  [host]
+  [auth host]
   (wcar* (car/del host)))
+
+(defn list-hosts
+  "list of known hosts"
+  nil)
+
+
+(comment
+  Tinymasq, Copyright 2013 Ronen Narkis, narkisr.com
+  Licensed under the Apache License,
+  Version 2.0  (the "License") you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.)
--- test/tinymasq/test/store.clj
+++ test/tinymasq/test/store.clj
@@ -1,21 +1,25 @@
 (ns tinymasq.test.store
-  (:use midje.sweet)  
+  (:use midje.sweet)
   (:import java.lang.AssertionError)
-  (:require 
+  (:require
     [tinymasq.store :refer (add-host del-host update-host get-host)]))
 
 (fact "legal hosts storage"
-   (del-host "foo")
-   (add-host "foo" "1.2.3.4") => "OK"
+   (del-host "password" "foo")  => (throws Exception)
+   (add-host "fooowner/secretausce" "foo" "1.2.3.4"   ) => "OK"
+   (add-host "bob/secretsauce" "bar" "4.5.6.7" "Welcome to Bob's Country Bunker!" ) => OK
    (get-host "foo") => "1.2.3.4"
-   (update-host "foo" "1.2.3.5") => "OK"
+   (update-host "fooowner/secretausce"  "foo" "1.2.3.5") => "OK"
+   (update-host "fooowner/secretausce"  "foo" "1.2.3.5"   "This is the awesome baz-service") => "OK"
    (get-host "foo") => "1.2.3.5"
+   (list-hosts) => OK
   )
 
 (fact "faulty hosts storage"
    (del-host "foo")
    (add-host "foo" "1.2.3.4") => "OK"
    (add-host "foo" "1.2.3.4") => (throws Exception)
+   (add-host "somepwf" "baz.blerk" "8.8.8.8") => (throws Exception) ;subdomains are not permitted
    (update-host "foo" "1.2.3.5") => "OK"
    (del-host "bar")
    (update-host "bar" "1.2.3.5") => (throws Exception))
